afcdea0ba736aebf6696a660b4aa449bd7400e6b
vasadisasidhar <vasadi.sasidhar92>
2024-09-11 14:41:33 -0700

    5th Commit

--- src/Components/Sampleprojects/Calculator/Calculatordemo.jsx
+++ src/Components/Sampleprojects/Calculator/Calculatordemo.jsx
@@ -1,47 +1,34 @@
 import React, { useState } from 'react';
 import './Calculator.css'; 
 
-const numbers =[1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-const operators = ['+', '-', '*', '/']
-
+const buttons = [1, 2, 3, '+', 4, 5, 6, '-', 7, 8,9, '*', 0, '.','CE','/'];
 const Calculatordemo = () => {
     const [input, setInput] = useState('');
-    const [result, setResult] = useState('');
-
     const handleButtonClick = (value) => {
         setInput(prev => prev + value);
     };
 
     const handleResultClick = () => {
         try {
-            setResult(eval(input));
+            setInput(eval(input));
         } catch {
-            setResult('Error');
+            setInput('Error');
         }
     };
-
     const handleClearClick = () => {
         setInput('');
-        setResult('');
     };
 
     return (
         <div className="calculator">
             <h1>Calculator</h1>
-            <input type="text"className="input"value={input}onChange={(e) => setInput(e.target.value)}/>
-            <h2 className="result">Result: {result}</h2>
-
+            <input type="text"className="input"value={input}onChange={(e) => setInput(e.target.value)}readOnly/>
             <div className="buttons">
-            {numbers.map(num => (
-                <button key={num} className="button" onClick={() => handleButtonClick(num)}>{num}</button>
-                    ))}
-
-            {operators.map(op => (
-                <button key={op} className="button" onClick={() => handleButtonClick(op)}>{op}</button>
+                {buttons.map((e) => (
+                    <button className="button" onClick={() => handleButtonClick(e)}>{e}</button>
                 ))}
-                
-                <button className="button" onClick={handleResultClick}>=</button>                
-                <button className="button clear-button" onClick={handleClearClick}>Clear</button>
+                <button className='button clear-button' onClick={handleClearClick}>C</button>
+                <button className="button result-button" onClick={handleResultClick}>=</button>
             </div>
         </div>
     );
